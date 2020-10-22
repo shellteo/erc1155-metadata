15fe6675dadd289987f981eb873364118e62789e
shellteo <shellteo@163.com>
2020-10-22 16:20:50 +0800

    update data

--- app/metadata/index.js
+++ app/metadata/index.js
@@ -1,13 +1,65 @@
 'use strict';
 
-const m1 = require('./1.json');
-const m2 = require('./2.json');
-const m3 = require('./3.json');
-const m4 = require('./4.json');
+// referral
+const c1 = require('./referral/1.json');
+const c2 = require('./referral/2.json');
+const c3 = require('./referral/3.json');
+
+// identity
+const c4 = require('./identity/4.json');
+const c5 = require('./identity/5.json');
+const c6 = require('./identity/6.json');
+const c7 = require('./identity/7.json');
+const c8 = require('./identity/8.json');
+const c9 = require('./identity/9.json');
+
+// referral
+const c10 = require('./referral/10.json');
+const c11 = require('./referral/11.json');
+const c12 = require('./referral/12.json');
+
+// volunteer
+const c13 = require('./volunteer/13.json');
+const c14 = require('./volunteer/14.json');
+const c15 = require('./volunteer/15.json');
+
+// governance
+const c16 = require('./governance/16.json');
+const c17 = require('./governance/17.json');
+const c18 = require('./governance/18.json');
+// timestamp
+const c19 = require('./timestamp/19.json');
+const c20 = require('./timestamp/20.json');
+const c21 = require('./timestamp/21.json');
+// game
+const c22 = require('./game/22.json');
+const c23 = require('./game/23.json');
+const c24 = require('./game/24.json');
+
 
 module.exports = {
-  1: m1,
-  2: m2,
-  3: m3,
-  4: m4,
+  1: c1,
+  2: c2,
+  3: c3,
+  4: c4,
+  5: c5,
+  6: c6,
+  7: c7,
+  8: c8,
+  9: c9,
+  10: c10,
+  11: c11,
+  12: c12,
+  13: c13,
+  14: c14,
+  15: c15,
+  16: c16,
+  17: c17,
+  18: c18,
+  19: c19,
+  20: c20,
+  21: c21,
+  22: c22,
+  23: c23,
+  24: c24,
 };
